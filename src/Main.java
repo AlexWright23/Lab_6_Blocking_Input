04be0e1570b341966b46411b4d541c93327cd6ed
AlexWright23 <honeybadger937@gmail.com>
2023-10-15 16:52:43 -0400

    Task 4 done.

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,42 @@
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double height = 0;
-        double width = 0;
+        double inch = 39.37;
+        double foot = 3.28;
+        double metric = 0;
+        double mile = 1609.35;
 
-        boolean valInput = false;
+        String inches = "Inches";
+        String feet = "Feet";
+        String miles = " Miles";
+        String trash = "";
 
-        do {
-            System.out.print("Please enter the height of the rectangle: ");
-            if (scanner.hasNextDouble()) {
-                height = scanner.nextDouble();
-                valInput = true;
-            } else {
-                System.out.println("Invalid input. Please enter a valid number");
-                scanner.next();
-            }
-        }while (!valInput);
-
-        valInput = false;
+        boolean done = false;
 
         do {
-            System.out.print("Please enter the width of the rectangle: ");
+            System.out.println("Please enter the measurement in meters: ");
             if (scanner.hasNextDouble()) {
-                width = scanner.nextDouble();
-                valInput = true;
+                metric = scanner.nextDouble();
+                done = true;
+                scanner.nextLine();
             } else {
-                System.out.println("Invalid input. Please enter a valid number");
-                scanner.next();
+                trash = scanner.nextLine();
+                System.out.println("Please enter a valid input, not " + trash);
             }
-        }while (!valInput);
+        } while (!done);
 
-        valInput = false;
+        double convertedInches = metric * inch;
+        double convertedFeet = metric * foot;
+        double convertedMiles = metric * mile;
 
-        double area = height * width;
-        System.out.println("Area of the rectangle is: " + area);
-
-        double perimeter = 2 * (height * width);
-        System.out.println("The perimeter of the rectangle is: " + perimeter);
-
-        double diagonalLength =  java.lang.Math.sqrt(width * width + height * height);
-        System.out.println("The length of the diagonal is: " + diagonalLength);
-
-        scanner.close();
+        System.out.printf("%-12s: %-12.3f%n", inches, convertedInches);
+        System.out.printf("%-12s: %-12.3f%n", feet, convertedFeet);
+        System.out.printf("%-12s: %-12.3f%n", miles, convertedMiles);
     }
+
 }
 
